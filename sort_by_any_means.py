506efb0a2b942055727f40cd6d98f6fefbac57e3
CROW_07 <100528047+HuyusakiYasiti@users.noreply.github.com>
2022-11-09 13:05:24 +0900

    Update sort_by_any_means.py

--- sort_by_any_means.py
+++ sort_by_any_means.py
@@ -1,55 +1,124 @@
-MAXIMUM = 4
+def main():
 
-n = int()
+    MAXIMUM_LENGTH = 4
 
-temporaryList = list(range(MAXIMUM))
-indexsList = list()
 
-def IndexRequest(value, answers):
-    choice = str()
-    answer = str()
+    temporaryList = list(range(MAXIMUM_LENGTH))
+
+    indexsList = list()
+
+
+    print(temporaryList)
+
+
+    while len(temporaryList) != 0:
+
+        n = temporaryList.pop(0)
+
+        indexsList.append(IndexRequest(n, indexsList, MAXIMUM_LENGTH))
+
+
+    temporaryList = SortAsOrdered(indexsList, MAXIMUM_LENGTH)
+
+    print(temporaryList)
+
+
+    temporaryList.sort()
+
+    print(temporaryList)
+
+
+
+
+
+
+
+
+def IndexRequest(value, answers, length):
 
     possibleNumbers = list()
 
-    for number in range(MAXIMUM):
+    choice = str()
+
+
+    for number in range(length):
+
         if number not in answers:
+
             possibleNumbers.append(number)
 
+
     for suitable in possibleNumbers:
-        choice = choice + ", {}".format(suitable)
-    choice = choice.lstrip(", ")
-    print("What is the index of '{}'?".format(value))
 
-    answer = input("Choose from {}.: ".format(choice))
+        choice = choice + "{}, ".format(suitable)
 
-    try:
-        answer = int(answer)
-    except ValueError:
-        print("please input number.")
-        answer = IndexRequest(value, answers)
-        return answer
 
-    if answer in possibleNumbers:
-        return answer
-    else:
-        print("'{}' is invalid number.".format(answer))
-        answer = IndexRequest(value, answers)
-        return answer
+    choice = choice.rstrip(", ")
 
-def SortAsOrdered(orders, i=0, orderedGoods=list(0 for x in range(MAXIMUM))):
-    for order in orders:
+
+
+
+
+    while True:
+
+        print("What is the index of '{}'?".format(value))
+
+
+        answer = input("Choose from {}.: ".format(choice))
+
+        try:
+
+            answer = int(answer)
+
+        except ValueError:
+
+            print("please input positive integer value.")
+
+            continue
+
+
+        if answer not in possibleNumbers:
+
+            print("'{}' is invalid number.".format(answer))
+
+            continue
+
+
+        break
+
+
+
+
+    return answer
+
+
+
+
+
+
+
+
+def SortAsOrdered(orders, length):
+
+    orderedGoods=list(0 for x in range(length))
+
+
+    for i, order in enumerate(orders):
+
         orderedGoods.insert(order, i)
+
         del orderedGoods[order + 1]
-        i = i + 1
+
+
     return orderedGoods
 
-print(temporaryList)
 
-while len(temporaryList) != 0:
-    n = temporaryList.pop(0)
-    indexsList.append(IndexRequest(n, indexsList))
-temporaryList = SortAsOrdered(indexsList)
 
-print(temporaryList)
-temporaryList.sort()
-print(temporaryList)
+
+
+
+
+if __name__ == "__main__":
+
+    main()
+
